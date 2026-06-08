afbdead2275bdac031c6902f7b618876c0f86402
Mostafa Tamiminezhad <130439123+M7tnj@users.noreply.github.com>
2026-06-08 03:06:08 +0000

    .

--- main.R
+++ main.R
@@ -2,6 +2,7 @@
 #### Packages Required ###
 #########################
 
+library(BiocManager)
 library(affy)
 library(affyPLM)
 library(limma)
@@ -46,13 +47,46 @@ library(tibble)
 library(tidyverse)
 library(ggridges)
 library(ReactomePA)
-library(ReactomePA)
-library(gsePathway)
 library(stringr)
 library(clusterProfiler)
 library(tabbycat)
 library(igraph)
 library(STRINGdb)
 library(patchwork)
+
+
+######################################
+##### Download & Import Samples #####
+####################################
+
+dirs <- c("results", "results/QC", "results/DEG", "results/PPI",
+          "results/Enrichment", "results/Modules", "results/GSEA",
+          "results/HubsOfHubs")
+for (d in dirs) dir.create(d, recursive = TRUE, showWarnings = FALSE)
+
+gse_id  <- "GSE8611"
+
+
+getGEOSuppFiles(GEO = gse_id, makeDirectory = TRUE)
+
+tar_file <- list.files(file.path( gse_id), pattern = "\\.tar$", full.names = TRUE)
+
+if (length(tar_file) > 0) {
+  untar(tar_file, exdir = file.path(gse_id, "CEL"))
+  cel_path <- file.path( gse_id, "CEL")
+} else {
+  cel_path <- file.path(gse_id)
+}
+
+all <- list.files("GSE8611/CEL", pattern="\\.CEL\\.gz$", full.names=TRUE, ignore.case=TRUE)
+cat("Total files found:", length(all), "\n")
+
+# Files of interest
+keep <- c("69","59","63","19","23","25")
+pattern <- paste0("GSM\\d*(",paste(keep,collapse="|"),")\\.CEL\\.gz$")
+del <- all[!grepl(pattern, basename(all), ignore.case=TRUE)]
+file.remove(del)
+
+
 
 
